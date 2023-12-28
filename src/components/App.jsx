a8b6b0484f47cef9d2e354bbeadc86750aea6adb
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-12-28 10:09:12 +0200

    add images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import './App.css';
+import Navbar from './Navbar/Navbar';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+     <Navbar/>
     </div>
   );
 };
