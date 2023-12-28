17f818319971947d94767d46f3fa30bd572e882f
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-12-28 10:53:11 +0200

    add navbar markup, styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import './Navbar.css'
+import React, { useState } from 'react';
+import './Navbar.css';
+import logo from '../assets/logo.png';
+import cart_icon from '../assets/cart_icon.png';
 
 const Navbar = () => {
+
+    const [menu, setMenu] = useState("shop");
   return (
-    <div className='navbar'>Navbar</div>
+      <div className='navbar'>
+          <div className="nav-logo">
+              <img src={logo} alt="Logo" />
+              <p>Store</p>
+          </div>
+          <ul className="nav-menu">
+              <li onClick={() => { setMenu("shop") }}>Shop {menu === "shop"? <hr/> : <></>}</li>
+              <li onClick={()=>{setMenu("men")}}>Men{menu === "men"? <hr/> : <></>}</li>
+              <li onClick={()=>{setMenu("women")}}>Women{menu === "women"? <hr/> : <></>}</li>
+              <li onClick={()=>{setMenu("kids")}}>Kids{menu === "kids"? <hr/> : <></>}</li>
+          </ul>
+          <div className="nav-login-cart">
+              <button>Login</button>
+              <img src={cart_icon} alt="Cart-icon" />
+              <div className='nav-cart-count'>0</div>
+          </div>
+    </div>
   )
 }
 
