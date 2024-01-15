9b7102d3e5e482b54af9815dfa42daaefaf127e9
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-01-15 18:57:54 +0200

    add NewsLetter markup, styles

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,5 +1,6 @@
 import Hero from 'components/Hero/Hero'
 import NewCollections from 'components/NewCollections/NewCollections'
+import NewsLetter from 'components/NewsLetter/NewsLetter'
 import Offers from 'components/Offers/Offers'
 import Popular from 'components/Popular/Popular'
 import React from 'react'
@@ -10,7 +11,8 @@ const Shop = () => {
       <Hero />
       <Popular />
       <Offers />
-      <NewCollections/>
+      <NewCollections />
+      <NewsLetter/>
     </div>
   )
 }
