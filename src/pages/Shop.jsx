6c583e56dcb874b48df3cbb5a52130e7217adc65
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-01-15 18:36:52 +0200

    add newCollections markup, styles

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,5 @@
 import Hero from 'components/Hero/Hero'
+import NewCollections from 'components/NewCollections/NewCollections'
 import Offers from 'components/Offers/Offers'
 import Popular from 'components/Popular/Popular'
 import React from 'react'
@@ -8,7 +9,8 @@ const Shop = () => {
     <div>
       <Hero />
       <Popular />
-      <Offers/>
+      <Offers />
+      <NewCollections/>
     </div>
   )
 }
