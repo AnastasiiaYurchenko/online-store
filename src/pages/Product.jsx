c470a8c7f1006bd9f857c404986f8216d4b44671
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-01-16 16:24:26 +0200

    start Product markup(done Breadcrum )

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import Breadcrum from 'components/Breadcrums/Breadcrum';
+import { ShopContext } from 'context/ShopContext'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom';
 
 const Product = () => {
+
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e)=>e.id === Number(productId))
   return (
-    <div>Product</div>
+    <div>
+      <Breadcrum product={product} />
+    </div>
   )
 }
 
