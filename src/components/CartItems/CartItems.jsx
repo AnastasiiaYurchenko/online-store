e884cc2a5159e2af0920f7a58f5e2ac7f2d3f01a
AnastasiiaYurchenko <nastya.brok@ukr.net>
2024-01-24 10:13:46 +0200

    add CartItems styles. markup

--- src/components/CartItems/CartItems.jsx
+++ src/components/CartItems/CartItems.jsx
@@ -21,13 +21,13 @@ const CartItems = () => {
               {
                   return (
                         <div>
-                            <div className="cartitems-format">
-                                <img src={e.image} alt="Product" className='carticon' />
+                            <div className="cartitems-format cartitems-format-main">
+                                <img src={e.image} alt="Product" className='carticon-product-icon' />
                                 <p>{e.name}</p>
                                 <p>{e.new_price}</p>
                                 <button className='cartitems-quantity'>{cartItems[e.id]}</button>
-                                <p>{e.new_price*cartItems[e.id]}</p>
-                                <img src={remove_icon} onClick={()=>removeFromCart(e.id)} alt="Remove Icon" />
+                                <p>${e.new_price*cartItems[e.id]}</p>
+                                <img className='carticon-remove-icon' src={remove_icon} onClick={()=>removeFromCart(e.id)} alt="Remove Icon" />
                             </div>
                             <hr />
                       </div>
@@ -35,6 +35,35 @@ const CartItems = () => {
               }
               return null;
           })}
+          <div className="cartitems-down">
+              <div className="cartitems-total">
+                  <h1>cart Totals</h1>
+                  <div>
+                      <div className="cartitems-total-item">
+                          <p>Subtotal</p>
+                          <p>${0}</p>
+                      </div>
+                      <hr />
+                      <div className="cartitems-total-item">
+                          <p>Shipping Fee</p>
+                          <p>Free</p>
+                      </div>
+                      <hr />
+                      <div className="cartitems-total-item">
+                          <h3>Total</h3>
+                          <h3>${0}</h3>
+                      </div>
+                  </div>
+                  <button>Proceed to Checkout</button>
+              </div>
+              <div className="cartitems-promocode">
+                  <p>If you have a promo code, Enter it here</p>
+                  <div className="cartitems-promobox">
+                      <input type="text" placeholder='Promo code' />
+                      <button>Submit</button>
+                  </div>
+              </div>
+          </div>
     </div>
   )
 }
